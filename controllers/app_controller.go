78014b73a0fe33390771aba01af30a95e0c6480f
GKingJone <990108852@qq.com>
2022-10-12 20:45:14 +0800

    第一次提交

---
+++ controllers/app_controller.go
@@ -0,0 +1,155 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"github.com/kubebuilder-demo/controllers/utils"
+	_ "k8s.io/api/extensions/v1beta1"
+	netingress "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	zhanggqtechv1beta1 "github.com/kubebuilder-demo/api/v1beta1"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+// AppReconciler reconciles a App object
+type AppReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=zhanggq.tech.zhanggq.tech,resources=apps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=zhanggq.tech.zhanggq.tech,resources=apps/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=zhanggq.tech.zhanggq.tech,resources=apps/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the App object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
+func (r *AppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	// TODO(user): your logic here
+	//首先获取app资源对象
+	app := &zhanggqtechv1beta1.App{}
+	//从缓存中获取app
+	if err := r.Get(ctx, req.NamespacedName, app); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+
+	}
+	//根据app的配置进行处理
+	//1.Deployment的处理
+	deployment := utils.NewDeployment(app)
+
+	if err := controllerutil.SetControllerReference(app, deployment, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	//查找同名deployment
+	d := &v1.Deployment{}
+	if err := r.Get(ctx, req.NamespacedName, d); err != nil {
+		if errors.IsNotFound(err) {
+			if err := r.Create(ctx, deployment); err != nil {
+				logger.Error(err, "create deployment failed")
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		if err := r.Update(ctx, deployment); err != nil {
+			return ctrl.Result{}, err
+		}
+
+	}
+	//2.service的处理逻辑
+	service := utils.NewService(app)
+	if err := controllerutil.SetControllerReference(app, service, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	//查找service
+	s := &corev1.Service{}
+	if err := r.Get(ctx, types.NamespacedName{Name: app.Name, Namespace: app.Namespace}, s); err != nil {
+		if errors.IsNotFound(err) && app.Spec.EnableServer {
+			if err := r.Create(ctx, service); err != nil {
+				logger.Error(err, "create service failed")
+				return ctrl.Result{}, err
+			}
+		}
+		if !errors.IsNotFound(err) && app.Spec.EnableServer {
+			return ctrl.Result{}, err
+		}
+	} else {
+		if app.Spec.EnableServer {
+			logger.Info("skip update")
+		} else {
+			if err := r.Delete(ctx, s); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	}
+	//3.Ingress处理逻辑
+	ingress := utils.NewIngress(app)
+	if err := controllerutil.SetControllerReference(app, ingress, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	i := &netingress.Ingress{}
+	if err := r.Get(ctx, types.NamespacedName{Name: app.Name, Namespace: app.Namespace}, i); err != nil {
+		if errors.IsNotFound(err) && app.Spec.EnableIngress {
+			if err := r.Create(ctx, ingress); err != nil {
+				logger.Error(err, "create ingress failed")
+				return ctrl.Result{}, err
+			}
+
+		}
+		if !errors.IsNotFound(err) && app.Spec.EnableIngress {
+			return ctrl.Result{}, err
+		}
+	} else {
+		if app.Spec.EnableIngress {
+			logger.Info("skip update")
+		} else {
+			if err := r.Delete(ctx, i); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+// owns设置之后，deployment以及ingress和service发生变化后reconsiler就会调用
+func (r *AppReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&zhanggqtechv1beta1.App{}).
+		Owns(&v1.Deployment{}).
+		Owns(&netingress.Ingress{}).
+		Owns(&corev1.Service{}).
+		Complete(r)
+}
